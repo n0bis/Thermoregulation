2655beaf2daab193a37fb3bca7a922e2b8f2b0b1
n0bis <mafal17@student.sdu.dk>
2022-04-13 12:16:28 +0200

    temperaturify the data
    fixes for #11 & #1 - leaking stuff in bash script :-1:

--- neo4j/seed/init.sh
+++ neo4j/seed/init.sh
@@ -9,23 +9,25 @@ set -m
 
 /docker-entrypoint.sh neo4j &
 
+sleep 60
+
 log_info "Waiting until neo4j stats at :7474 ..."
-wget --quiet --tries=10 --waitretry=30 -O /dev/null bolt://localhost:7474
-wget --quiet --tries=10 --waitretry=30 -O /dev/null bolt://localhost:7687
+wget -b --quiet --tries=10 --waitretry=30 -O /dev/null bolt://localhost:7474
 if [ -d "/cyphers" ]; then
   log_info  "Deleting all relations"
-  cypher-shell --format plain "MATCH (n) DETACH DELETE n"
+  cypher-shell --format plain "MATCH (n) DETACH DELETE n" -u neo4j -p 2cool4school
 
   log_info  "Wrapper: Loading cyphers from '/cyphers'"
   for cipherFile in /cyphers/*.cql; do
       log_info "Running cypher ${cipherFile}"
-      contents=$(cat ${cipherFile})
-      cypher-shell -a bolt://localhost:7687 "${contents}"
+      cypher-shell --file $cipherFile -u neo4j -p 2cool4school
+      #contents=$(cat ${cipherFile})
+      #cypher-shell -a bolt://localhost:7687 "${contents}"
   done
   log_info  "Finished loading all cyphers from '/cyphers'"
 fi
 
-TOTAL_CHANGES=$(cypher-shell --format plain "MATCH (n) RETURN count(n) AS count")
+TOTAL_CHANGES=$(cypher-shell --format plain "MATCH (n) RETURN count(n) AS count" -u neo4j -p 2cool4school)
 # https://stackoverflow.com/questions/15520339/how-to-remove-carriage-return-and-newline-from-a-variable-in-shell-script/15520508#15520508
 log_info "Wrapper: Changes $(echo ${TOTAL_CHANGES} | sed -e 's/[\r\n]//g')"
 
