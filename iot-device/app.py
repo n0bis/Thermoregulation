45a28e1f5c1078d7e1bfecd6bca267882c6233f1
n0bis <mafal17@student.sdu.dk>
2022-05-12 09:16:41 +0200

    publish to that mqtt

--- iot-device/app.py
+++ iot-device/app.py
@@ -1,15 +1,14 @@
 import paho.mqtt.client as mqtt 
 from random import randrange, uniform
 from time import sleep, time
-
-mqttBroker = "tcp://mqtt:1883" 
+import json
 
 client = mqtt.Client("Temperature_Inside")
-client.connect(mqttBroker) 
+client.connect("localhost", 1883, 60) 
 
 while True:
     randNumber = randrange(0, 21.0)
     event = {"value": randNumber, "timestamp": int(time())}
-    client.publish("TEMPERATURE", event)
-    print(f"Just published {event} to topic TEMPERATURE")
+    client.publish("TEMPERATURE", json.dumps(event))
+    print(f'Just published {json.dumps(event)} to topic TEMPERATURE')
     sleep(5)
