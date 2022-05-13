7f927da89afe01bb928a7af119c3b642b09e9a18
n0bis <mafal17@student.sdu.dk>
2022-05-13 09:50:18 +0200

    keep dsl generated code

---
+++ dsl/runtime-EclipseXtext/test/src-gen/rules/app.py
@@ -0,0 +1,58 @@
+from neo4j import GraphDatabase
+from confluent_kafka import Consumer
+import paho.mqtt.client as mqtt 
+import os
+import json
+
+uri = "neo4j://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=(os.getenv('NEO4J_USER', 'neo4j'), os.getenv('NEO4J_PASSWORD', '2cool4school')))
+session = driver.session()
+client = mqtt.Client("rules_engine")
+client.connect("localhost", 1883, 60) 
+
+consumer = Consumer({
+	'group.id': 'foo',
+    'bootstrap.servers': 'localhost',
+    'auto.offset.reset': 'latest'
+})
+consumer.subscribe(['temperature'])
+
+maxTemperature = 21
+minTemperature = 1
+
+with driver.session() as session:
+	session.run(("CREATE (rule:Rule {nameSpace: 'apples', "
+		"min: "+ str(minTemperature) +", "
+		"max: "+ str(maxTemperature) + "})"
+	))
+
+hasRuleBeenBroken = False # Make state machine
+while True:
+	msg = consumer.poll(1.0)
+
+	if msg is None:
+		continue
+	if msg.error():
+		print("Consumer error: {}".format(msg.error()))
+		continue
+	
+	try:
+		print('Received message: {}'.format(msg.value().decode('utf-8')))
+		event = json.loads(msg.value().decode('utf-8'))
+		if minTemperature > event["value"] and not hasRuleBeenBroken:
+			client.publish("rules/alert", "LEDblink: red")
+			print("Too cold! - publish to mqtt")
+			hasRuleBeenBroken = True
+		elif maxTemperature < event["value"] and not hasRuleBeenBroken:
+			client.publish("rules/alert", "LEDblink: green")
+			print("Too hot! - publish to mqtt")
+			hasRuleBeenBroken = True
+		else:
+			print("No rule broken move along - publish to mqtt")
+			client.publish("rules/alert", "")
+	except Exception as err:
+		print(err)
+		continue
+
+c.close()
+
