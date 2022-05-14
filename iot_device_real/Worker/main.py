678a2a0b9a4dde3401528b0bac8dd771a4e2eff5
Patrick <prik.n@hotmail.de>
2022-05-14 13:52:34 +0200

    Finished Worker code

--- iot_device_real/Worker/main.py
+++ iot_device_real/Worker/main.py
@@ -1,4 +1,4 @@
-from machine import Pin, RTC
+from machine import Pin, RTC, ADC
 from utime import sleep_ms, ticks_ms
 from BluetoothBroadcaster import ESP32_BLE
 from math import ceil
@@ -8,6 +8,8 @@ import gc
 
 gc.collect()
 
+print("main")
+
 # Update timings (Interval to send data is avg_count * sample_interval)
 sample_interval = 1  # Interval in which to sample the temperature !Should not be lower than 1!
 avg_count = 5      # How many samples to average before sending
@@ -16,6 +18,9 @@ avg_count = 5      # How many samples to average before sending
 t1 = dht.DHT11(Pin(27, Pin.IN))
 temp1 = []
 
+# Potentiometer
+pot = ADC(Pin(34), atten=ADC.ATTN_11DB)
+
 # Setup Bluetooth
 bt_bc = ESP32_BLE("ESP32BLE_reciever")
 bt_bc.scanForReciever()
@@ -23,12 +28,14 @@ bt_bc.scanForReciever()
 # Storage in case of connection errors
 temp_store = []
 
+# Helper method to calculate free memory size
 def freeMem():
     f = gc.mem_free()
     a = gc.mem_alloc()
     t = f+a
     return (f/t * 100)
 
+# Sends a message over the established BLE connection
 def send_data(msg):
     print("sending message: %s" %(msg))
     try:
@@ -37,12 +44,14 @@ def send_data(msg):
         bt_bc.disconnect()
         print("Connection lost")
 
+# Sends cumulated data collected during connection-loss
 def send_stored_data():
     for d in temp_store:
         msg = create_msg(d)
         send_data(msg)
         sleep_ms(1000)
     temp_store.clear()
+    gc.collect()
         
 
 def create_msg(temps):
@@ -52,10 +61,12 @@ def create_msg(temps):
 sample_count = 0
 sleep_ms(2000)
 
+
 # Main Loop
 while True:
     sleep_ms(sample_interval*1000)
-    
+    modifier = ceil((pot.read()/100)-25)
+
     t1.measure()
     temp1.append(t1.temperature())
     sample_count += 1
@@ -64,11 +75,12 @@ while True:
     
     if sample_count == avg_count:
         timestamp = time.time()
-        print(timestamp)
         avg_temp = (sum(temp1)/ avg_count)
         temp1.clear()
+        gc.collect()
         
-        current_temp = [timestamp, avg_temp]
+        current_temp = [timestamp, avg_temp+modifier]
+        print(current_temp)
         
         if bt_bc.isConnected():
             if len(temp_store) > 0:
