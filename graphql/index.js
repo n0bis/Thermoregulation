8b0187f9cc6956b0db38a41a50a925d1249e2785
n0bis <mafal17@student.sdu.dk>
2022-05-17 17:59:34 +0200

    send sensor data

--- graphql/index.js
+++ graphql/index.js
@@ -9,11 +9,16 @@ const typeDefs = gql`
     name: String
   }
 
+  type Sensor {
+    id: ID! @id
+    truck: [Truck!]! @relationship(type: "TRACKED_BY", direction: OUT)
+  }
+
   type Temperature {
     id: ID! @id
     value: Float
     tracked_at: Int
-    truck: [Truck!]! @relationship(type: "TRACKED_BY", direction: OUT)
+    sensor: [Sensor!]! @relationship(type: "OWNED_BY", direction: OUT)
   }
 
   type Rule {
--- iot-device/app.py
+++ iot-device/app.py
@@ -7,8 +7,15 @@ client = mqtt.Client("Temperature_Inside")
 client.connect("localhost", 1883, 60) 
 
 while True:
-    randNumber = randrange(0, 21.0)
-    event = {"value": randNumber, "timestamp": int(time())}
-    client.publish("TEMPERATURE", json.dumps(event))
-    print(f'Just published {json.dumps(event)} to topic TEMPERATURE')
+    for id in range(1, 5):
+        randNumber = randrange(0, 21.0)
+        event = {
+            "id": id,
+            "temperature": {
+                "value": randNumber
+            }, "humidity": {
+                "level": 50.00
+        }, "timestamp": int(time())}
+        client.publish("TEMPERATURE", json.dumps(event))
+        print(f'Just published {json.dumps(event)} to topic TEMPERATURE')
     sleep(5)
